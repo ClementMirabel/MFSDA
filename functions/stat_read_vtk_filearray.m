e63912a0090f57225ffaa9c1c7ed6498bc9b193d
Juan Carlos Prieto <juanprietob@gmail.com>
2017-05-22 14:46:27 -0400

    BUG: Use notation {} to get the filename from the array

--- functions/stat_read_vtk_filearray.m
+++ functions/stat_read_vtk_filearray.m
@@ -1,6 +1,6 @@
 function vertex = stat_read_vtk_filearray(FileNames)    
     nn=size(FileNames,1);
     for ii=1:nn
-        vertex(ii,:,:) = stat_read_vtk_file(FileNames(ii));
+        vertex(ii,:,:) = stat_read_vtk_file(FileNames{ii});
     end
 end
