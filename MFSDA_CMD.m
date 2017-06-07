dceff9634ce5531e7798ac81498433264f197450
Juan Carlos Prieto <juanprietob@gmail.com>
2017-06-07 14:38:55 -0400

    BUG: use full path to write json files

--- MFSDA_CMD.m
+++ MFSDA_CMD.m
@@ -1,19 +1,29 @@
-function MFSDA_CMD(ShapeDataName, CoordDataName, CovariatesName, CovariateofInterestName, CovariateTypeName, OutputDir)       
+function MFSDA_CMD(ShapeDataName, CoordDataName, CovariatesName, CovariateofInterestName, CovariateTypeName, OutputDir, exportJSON)
     
     [pathstr,~,~] = fileparts(mfilename('fullpath'));
     addpath(genpath([pathstr,'/data']));
-    addpath(genpath([pathstr,'/functions']));
+    addpath(genpath([pathstr,'/functions']));    
     
     try
 
         ShapeDataFileArray = importdata(ShapeDataName);    
-        MFSDA(ShapeDataFileArray, CoordDataName, CovariatesName, CovariateofInterestName, CovariateTypeName, OutputDir);        
-    catch
-        fprintf('Error running MFSDA, please check input data');
+        outputFiles = MFSDA(ShapeDataFileArray, CoordDataName, CovariatesName, CovariateofInterestName, CovariateTypeName, OutputDir);
+        
+        if(exist('exportJSON') && exportJSON)
+            addpath(genpath([pathstr,'/jsonlab-1.5']));            
+            cellfun(@(filename)exportToJSON(filename), outputFiles);
+        end
+    catch e
+        fprintf('Error running MFSDA, please check input data: %s', e.message);
     end
     
-    exit
-
-    
-    
-    
+function exportToJSON(filename)
+    try
+        [path, name, ~] = fileparts(filename);
+        jsonoutputfilename = fullfile(path, [name '.json']);
+        fprintf('\n+++Exporting %s to JSON+++\n', filename);
+        
+        savejson('', load(jsonoutputfilename), );
+    catch e
+        fprintf('\nError exporting to JSON, please check output data: %s\n', e.message);
+    end
